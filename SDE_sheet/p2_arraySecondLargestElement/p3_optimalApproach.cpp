fe58e04e6ffeab3847408535de10f18bc7cd7aa8
DrunkTrader <neerajrajputa786@gmail.com>
2025-02-17 23:39:51 +0530

    minor changes

--- SDE_sheet/p2_arraySecondLargestElement/p3_optimalApproach.cpp
+++ SDE_sheet/p2_arraySecondLargestElement/p3_optimalApproach.cpp
@@ -18,16 +18,24 @@ int secondSmallest(int arr[], int n){
     return second_smallest;
 }
 
-int secondLargest(int arr[], int n){
+int secondLargest(vector<int> &arr){}{
+    int n = sizeof(arr)/sizeof(arr[0]);
     int largest_number = INT_MIN;
     int second_largest = INT_MIN;
 
     for(int i = 0; i < n; i++){
-        if (arr[i] > largest_number){
+        if (n == 0 || n == 1){                    //base case
+            cout << -1 << " "<< -1 << endl;
+            return -1;
+
+        } else if (arr[i] > largest_number){
             second_largest = largest_number;
             largest_number = arr[i];
         }
         else if(arr[i] > second_largest && arr[i] != largest_number){
+            second_largest = arr[i];
+        }
+        else if (largest_number == second_largest){
             second_largest = arr[i];
         }
     }
@@ -35,15 +43,10 @@ int secondLargest(int arr[], int n){
 }
 
 int main(){
-    int arr1[] = {1};
-    int size = sizeof(arr1)/sizeof(arr1[0]);
-    if (size == 0 || size == 1){                    //base case
-        cout << -1 << " "<< -1 << endl;
-        return -1;
-    }
+    int arr1[] = {12, 35, 1, 10, 34, 1};
 
-    cout << "second smallest : " << secondSmallest(arr1, size) << endl;
-    cout << "second largest : " << secondLargest(arr1, size) << endl;
+    cout << "second smallest : " << secondSmallest(arr1) << endl;
+    cout << "second largest : " << secondLargest(arr1) << endl;
     return 0;
 }
 
