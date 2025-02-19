d552a4774af16ae13c8450915cec8278218b3621
DrunkTrader <neerajrajputa786@gmail.com>
2025-02-19 20:02:51 +0530

    check sorted array

--- SDE_sheet/p3_arrayCheckSortedOrNot/p1_bruteApproach.cpp
+++ SDE_sheet/p3_arrayCheckSortedOrNot/p1_bruteApproach.cpp
@@ -1,11 +1,21 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    float a = 10.01;
-    double b = static_cast<double>(a);  // Converts int to double
-    cout << "b: " << b << endl;
-    cout << typeid(a).name() << endl;
-    cout << typeid(b).name() << endl;
+bool checksorted(int arr[], int n){
+    if(n == 0 || n == 1) return true;   // if array is empty or has only one element
+    for(int i = 0; i < n-1; i++){       // if array has more than one element
+        if (arr[i] <= arr[i+1]){        // if current element is less than or equal to next element
+            return true;                // then array is sorted
+        }
+        else{                               // if current element is greater than next element
+            return false;                // then array is not sorted
+        }
+    }
+}
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bool result = checksorted(arr, n);
+    cout << (result ? "Array is sorted" : "Array is not sorted") << endl;
     return 0;
 }
