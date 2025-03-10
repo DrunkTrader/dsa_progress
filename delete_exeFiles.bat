82afea2f4e52e80c132a8a9d00faa9d992a500ef
DrunkTrader <neerajrajputa786@gmail.com>
2025-03-10 17:29:16 +0530

    exe file remover added

---
+++ delete_exeFiles.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Deletes all .exe files in D:\DSA and its subdirectories
+echo Deleting all .exe files in D:\DSA...
+del /S /Q "D:\DSA\*.exe"
+
+:: Confirmation message
+echo Deletion complete.
+pause
