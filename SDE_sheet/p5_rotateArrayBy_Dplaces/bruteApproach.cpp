b084883484be4abe8e690247cf8d9e13a91a602c
DrunkTrader <neerajrajputa786@gmail.com>
2025-03-12 17:41:25 +0530

    Rotate by D places file added (brute approach)

---
+++ SDE_sheet/p5_rotateArrayBy_Dplaces/bruteApproach.cpp
@@ -0,0 +1,44 @@
+/*
+  author: drunktrader aka Neeraj Kumar
+  created: 2025-03-12 17:04:55
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotateByD(vector<int> arr, int d){
+    int n = arr.size();
+
+    /*step 1 : compute the effective rotation
+    step 2 : create a temporary array and store the first D elements in the temporary array
+    step 3 : left push the elements by d places in the main array
+    step 4 : push the elements in the temporary array in the main array*/
+
+    d = d % n;      //compute the effective rotation
+    vector<int> tempArr(d);
+    for(int i = 0; i < d; i++){
+        tempArr[i] = arr[i];
+    }
+    //left push the elements in the main array
+    for(int i = 0; i <= n-d; i++){
+        arr[i] = arr[i + d];
+    }
+    // pushing the temp elements in the main array
+    for(int i = 0; i < d; i++){
+        arr[ n-d + i] = tempArr[i];
+    }
+    //print the array
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);   // fast I/O
+    
+    vector<int> arr1 = {1,2,3,4,5,6,7};
+
+    leftRotateByD(arr1, 3);
+    return 0;
+}
