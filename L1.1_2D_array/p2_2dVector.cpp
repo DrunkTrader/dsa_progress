4f46276c339be0b494c3e3bb20dc21bfa1bfcd34
DrunkTrader <neerajrajputa786@gmail.com>
2025-03-27 00:49:07 +0530

    codeforce contest codes

---
+++ L1.1_2D_array/p2_2dVector.cpp
@@ -0,0 +1,18 @@
+/*
+  author: drunktrader aka Neeraj Kumar
+  created: 2025-03-23 09:39:35
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void takeInput(vector<vector<int>> &v){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);   // fast I/O
+    
+    
+    return 0;
+}
