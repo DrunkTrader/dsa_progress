9e8925c86e43e3a9c7d855025ee06a8bb46e9df7
DrunkTrader <neerajrajputa786@gmail.com>
2025-04-01 09:24:29 +0530

    intersection of 2 sorted array

---
+++ SDE_sheet/p10_setMatrixZero/p1_bruteApproach.cpp
@@ -0,0 +1,19 @@
+/*
+  author: drunktrader aka Neeraj Kumar
+  created: 2025-03-31 10:03:36
+*/
+/*Problem Statement : */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void bruteSetMatrix(vector<int>){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);   // fast I/O
+    
+    
+    return 0;
+}
--- SDE_sheet/p7_linearSearch/p1_optimalSearch.cpp
+++ SDE_sheet/p7_linearSearch/p1_optimalSearch.cpp
@@ -6,19 +6,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void linearSearch(vector<int> &arr, int target ){
+int linearSearch(vector<int> &arr, int target ){
     int n = arr.size();
 
     for(int i = 0; i < n; i++){
         if(arr[i] == target) return i;
     }
-
-    return -1
+    
+    return -1; // return -1 if target not found
 }
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(0);   // fast I/O
+    // ios::sync_with_stdio(false); cin.tie(0);   // fast I/O
     
+    cout << "Enter the size of the array: ";
+    int n; cin >> n;
+    vector<int> arr(n); // Resize the vector to size n
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
     
+    cout << "Enter the target value: " ;
+    int target ; cin>> target;
+
+    //print the target position
+    int position = linearSearch(arr, target);
+    if (position == -1) {
+        cout << "Target not found." << endl;
+    } else {
+        cout << "The target is at position : " << position << endl;
+    }
+    cout << "Program completed successfully." << endl;
     return 0;
 }
